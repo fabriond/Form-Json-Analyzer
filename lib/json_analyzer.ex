5ff0a28a9117b91017b2a349d799350b083a549a
fabriond <gfbf@ic.ufal.br>
2019-03-20 22:11:48 -0300

    Minor changes

--- lib/json_analyzer.ex
+++ lib/json_analyzer.ex
@@ -8,24 +8,25 @@ defmodule JsonAnalyzer do
 
   ## Examples
 
-      iex> JsonAnalyzer.hello()
-      :world
+      iex> JsonAnalyzer.analyze(IHM.json)
+      :ok
+      #=> Result.json updated
 
   """
-  def invert_tuple(map, tuple) do
-    new_key = elem(tuple, 1)
-    new_val = elem(tuple, 0)
-
-    Map.update(map, new_key, [new_val], fn val -> val ++ [new_val] end)
-  end
-
-  def analyze do
+  def analyze(path) when is_binary(path) do
     result =
-      File.read!("IHM.json")
+      File.read!(path)
       |> Poison.decode!()
       |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
       |> Enum.reduce(%{}, &invert_tuple(&2, &1))
 
     File.write!("Result.json", Poison.encode!(result, iodata: true, pretty: true))
+  end
+
+  defp invert_tuple(map, tuple) do
+    new_key = elem(tuple, 1)
+    new_val = elem(tuple, 0)
+
+    Map.update(map, new_key, [new_val], fn val -> val ++ [new_val] end)
   end
 end
